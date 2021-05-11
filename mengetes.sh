4ab3768fce2f4040abdba35c429a88abe031aea2
ExWitch <83410728+PointFool@users.noreply.github.com>
2021-05-11 12:22:43 +0700

    Update mengetes.sh

--- mengetes.sh
+++ mengetes.sh
@@ -105,7 +105,7 @@ case $wh in
 	echo ""
 	echo "Bikin Databasenya gan :'v"
 	echo " wait"
-	sleep 40
+	sleep 100
 	docker ps -a
 	sleep 3
 	docker exec -it $nama\_db mysql -h 127.0.0.1 -P 3306 -u root -p$pass -e "CREATE DATABASE "$namadb";"
